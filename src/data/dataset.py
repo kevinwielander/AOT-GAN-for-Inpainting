fix: masking

src/data/dataset.py
@@ -64,7 +64,7 @@ class InpaintingData(Dataset):
 
         # augment
         image = self.img_trans(image) * 2.0 - 1.0
-        mask = F.to_tensor(self.mask_trans(mask))
+        mask = F.to_tensor(self.mask_trans(mask)) * 255
 
         return image, mask, filename
 

src/test.py
@@ -1,7 +1,7 @@
 import importlib
 import os
 from glob import glob
-
+from pathlib import Path
 import numpy as np
 import torch
 from PIL import Image
@@ -17,9 +17,11 @@ def postprocess(image):
     return Image.fromarray(image)
 
 
-def main_worker(args, use_gpu=True):
-    # device = torch.device("cuda") if use_gpu else torch.device("cpu")
+def recursive_glob(root_dir, pattern):
+    return [str(path) for path in Path(root_dir).rglob(pattern)]
 
+
+def main_worker(args, use_gpu=True):
     # Model and version
     net = importlib.import_module("model." + args.model)
     model = net.InpaintGenerator(args).cuda()
@@ -28,14 +30,32 @@ def main_worker(args, use_gpu=True):
 
     # prepare dataset
     image_paths = []
-    for ext in [".jpg", ".png"]:
-        image_paths.extend(glob(os.path.join(args.dir_image, "*" + ext)))
+    for ext in ["*.jpg", "*.png", "*.JPEG"]:
+        image_paths.extend(recursive_glob(os.path.join(args.dir_image, args.data_train), ext))
     image_paths.sort()
-    mask_paths = sorted(glob(os.path.join(args.dir_mask, "*.png")))
+
+    mask_paths = sorted(glob(os.path.join(args.dir_mask, args.mask_type, "*.png")))
+    if not mask_paths:
+        print(f"No masks found in {os.path.join(args.dir_mask, args.mask_type)}")
+        print("Using default quarter mask")
+        h = w = args.image_size
+        default_mask = np.zeros((h, w), dtype=np.uint8)
+        default_mask[:h // 2, :w // 2] = 255
+
+        default_mask_path = os.path.join(args.outputs, "default_mask.png")
+        Image.fromarray(default_mask).save(default_mask_path)
+        mask_paths = [default_mask_path]
+
     os.makedirs(args.outputs, exist_ok=True)
 
+    if not image_paths:
+        raise ValueError(f"No images found in {os.path.join(args.dir_image, args.data_train)}")
+
     # iteration through datasets
-    for ipath, mpath in zip(image_paths, mask_paths):
+    for ipath in image_paths:
+        # Get random mask for each image
+        mpath = np.random.choice(mask_paths)
+
         image = ToTensor()(Image.open(ipath).convert("RGB"))
         image = (image * 2.0 - 1.0).unsqueeze(0)
         mask = ToTensor()(Image.open(mpath).convert("L"))

src/trainer/trainer.py
@@ -1,7 +1,7 @@
 import importlib
 import os
 from glob import glob
-
+import wandb
 import torch
 from data import create_loader
 from loss import loss as loss_module
@@ -17,6 +17,12 @@ class Trainer:
     def __init__(self, args):
         self.args = args
         self.iteration = 0
+        if self.args.global_rank == 0:
+            wandb.init(
+                project="aotgan-inpainting",
+                config=vars(args),
+                name=f"{args.model}_{args.data_train}_{args.mask_type}{args.image_size}"
+            )
 
         # setup data set and data loader
         self.dataloader = create_loader(args)
@@ -33,6 +39,10 @@ class Trainer:
 
         self.netD = net.Discriminator().cuda()
         self.optimD = torch.optim.Adam(self.netD.parameters(), lr=args.lrd, betas=(args.beta1, args.beta2))
+
+        if self.args.global_rank == 0:
+            wandb.watch(self.netG)
+            wandb.watch(self.netD)
 
         self.load()
         if args.distributed:
@@ -104,6 +114,7 @@ class Trainer:
 
             # in: [rgb(3) + edge(1)]
             pred_img = self.netG(images_masked, masks)
+
             comp_img = (1 - masks) * images + masks * pred_img
 
             # reconstruction losses
@@ -143,6 +154,28 @@ class Trainer:
                     self.writer.add_image("orig", make_grid((images + 1.0) / 2.0), self.iteration)
                     self.writer.add_image("pred", make_grid((pred_img + 1.0) / 2.0), self.iteration)
                     self.writer.add_image("comp", make_grid((comp_img + 1.0) / 2.0), self.iteration)
+
+            if self.args.global_rank == 0 and (self.iteration % self.args.print_every == 0):
+                # Log metrics
+                log_dict = {
+                    'iteration': self.iteration,
+                    'model_time': timer_model.release(),
+                    'data_time': timer_data.release()
+                }
+
+                # Log losses
+                for key, val in losses.items():
+                    log_dict[f'loss/{key}'] = val.item()
+
+                # Log images
+                log_dict.update({
+                    'images/mask': wandb.Image(make_grid(masks)),
+                    'images/original': wandb.Image(make_grid((images + 1.0) / 2.0)),
+                    'images/predicted': wandb.Image(make_grid((pred_img + 1.0) / 2.0)),
+                    'images/composite': wandb.Image(make_grid((comp_img + 1.0) / 2.0))
+                })
+
+                wandb.log(log_dict)
 
             if self.args.global_rank == 0 and (self.iteration % self.args.save_every) == 0:
                 self.save()